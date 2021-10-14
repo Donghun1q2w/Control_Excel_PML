5845f396752cded1f34e875476b1a6747c4a06dd
LEEDONGHUN <ldh0774@naver.com>
2021-10-14 22:13:15 +0900

    ReadExcel Method 추가

---
+++ PMLExcelControl.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Aveva.Core.PMLNet;
+using excel = Microsoft.Office.Interop.Excel;
+
+namespace PML.Excel.Control
+{
+    [PMLNetCallable()]
+    public class PMLExcelControl
+    {
+        [PMLNetCallable()]
+        public PMLExcelControl()
+        { }
+        [PMLNetCallable()]
+        public void Assign(PMLExcelControl that)
+        { }
+        [PMLNetCallable()]
+        public void ReadExcel(string Path)
+        {
+            
+            excel.Workbook wb = null;
+            excel.Worksheet ws = null;
+            excel.Application Application = new excel.Application();
+            wb = Application.Workbooks.Open(Path);
+            ws = wb.Worksheets.Item[1] as excel.Worksheet;
+            ws.Range["C3"].Value = "TEST";
+            ws.SaveAs(Path);
+            
+
+        }
+
+    }
+
+}
